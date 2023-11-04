0dbc18ca730fbda9009da28aece990b78e9aa243
Chase <112102369+chasecraft88@users.noreply.github.com>
2023-11-04 10:09:31 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,57 @@
-// Get the canvas and its context
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
+// Initialize canvas and context
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
-// Define game objects
+// Create player object
 const player = {
     x: canvas.width / 2,
-    y: canvas.height - 30,
-    width: 30,
-    height: 30,
-    speed: 5,
+    y: canvas.height / 2,
+    speed: 2,
 };
 
-// Load game images
+// Load images
 const backgroundImg = new Image();
-backgroundImg.src = "https://github.com/chasecraft88/Legendary/blob/2f031cc5b91efb878d1a1d3501a103e35bb5d573/assets/Img10.jpg";
+backgroundImg.src = 'https://raw.githubusercontent.com/chasecraft88/Legendary/2f031cc5b91efb878d1a1d3501a103e35bb5d573/assets/Img10.jpg';
 
 const playerImg = new Image();
-playerImg.src = "https://github.com/chasecraft88/Legendary/blob/2f031cc5b91efb878d1a1d3501a103e35bb5d573/assets/Img2.png";
+playerImg.src = 'https://raw.githubusercontent.com/chasecraft88/Legendary/2f031cc5b91efb878d1a1d3501a103e35bb5d573/assets/Img2.png';
 
-// Game loop
-function gameLoop() {
-    requestAnimationFrame(gameLoop);
-
+// Draw images on the canvas
+function draw() {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw the background
+    // Draw the background image
     ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
 
-    // Move the player
-    if (rightPressed && player.x < canvas.width - player.width) {
-        player.x += player.speed;
-    }
-    if (leftPressed && player.x > 0) {
-        player.x -= player.speed;
-    }
-
-    // Draw the player
-    ctx.drawImage(playerImg, player.x, player.y, player.width, player.height);
+    // Draw the player image
+    ctx.drawImage(playerImg, player.x, player.y, 64, 64); // Adjust the size as needed
 }
 
-// Handle keyboard input (same as before)
+// Update player position
+function update() {
+    // Move the player with arrow keys
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'ArrowUp') {
+            player.y -= player.speed;
+        }
+        if (event.key === 'ArrowDown') {
+            player.y += player.speed;
+        }
+        if (event.key === 'ArrowLeft') {
+            player.x -= player.speed;
+        }
+        if (event.key === 'ArrowRight') {
+            player.x += player.speed;
+        }
 
-// Start the game loop
-gameLoop();
+        // Redraw the canvas
+        draw();
+    });
+}
+
+// Initial draw
+draw();
+
+// Call update function
+update();
